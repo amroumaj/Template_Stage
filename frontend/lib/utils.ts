4109235c35d2a9f85c350df292aa4455f87a788c
amroumaj <amrou.majdoub@etudiant-e>
2024-07-09 21:03:11 +0100

    added the 2 sidebars

---
+++ frontend/lib/utils.ts
@@ -0,0 +1,58 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const getShapeInfo = (shapeType: string) => {
+  switch (shapeType) {
+    case "rect":
+      return {
+        icon: "/assets/rectangle.svg",
+        name: "Rectangle",
+      };
+
+    case "circle":
+      return {
+        icon: "/assets/circle.svg",
+        name: "Circle",
+      };
+
+    case "triangle":
+      return {
+        icon: "/assets/triangle.svg",
+        name: "Triangle",
+      };
+
+    case "line":
+      return {
+        icon: "/assets/line.svg",
+        name: "Line",
+      };
+
+    case "i-text":
+      return {
+        icon: "/assets/text.svg",
+        name: "Text",
+      };
+
+    case "image":
+      return {
+        icon: "/assets/image.svg",
+        name: "Image",
+      };
+
+    case "freeform":
+      return {
+        icon: "/assets/freeform.svg",
+        name: "Free Drawing",
+      };
+
+    default:
+      return {
+        icon: "/assets/rectangle.svg",
+        name: shapeType,
+      };
+  }
+};
